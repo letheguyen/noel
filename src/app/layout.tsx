daa0e72e629f10d693523955131b6c69b0c92b72
The Nguyen <nguyenlt@relipasoft.com>
2025-12-07 00:31:36 +0700

    feat: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
       snow.style.setProperty("--moveX", -20 + Math.random() * 40 + "vw");
 
       document.body.appendChild(snow);
-      setTimeout(() => snow.remove(), 16000);
+      setTimeout(() => snow.remove(), 30000);
     };
 
-    const interval = setInterval(createSnow, 4000);
+    const interval = setInterval(createSnow, 3000);
     return () => clearInterval(interval);
   }, []);
 
